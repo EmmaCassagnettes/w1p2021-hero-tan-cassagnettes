b1367fb78ed49a2f8ddaf1981ad35ad9720ef64b
tanamanda13 <tanamanda13@gmail.com>
2019-05-22 17:13:46 +0200

    page bedroom image fix

--- app/components/Bedroom.vue
+++ app/components/Bedroom.vue
@@ -4,21 +4,20 @@
     <br />
     <section class="pictures">
       <div class="picture" @click="showPicture">
-        <!-- <img v-bind:src="selectedImage" @click.once="randomImage"/> -->
         <img v-bind:src="selectedImage"/>
       </div>
       <div class="picture">
-        <img v-bind:src="selectedImage1" @click.once="randomImage1"/>
+        <img v-bind:src="selectedImage1" @click="showPicture1"/>
       </div>
       <div class="picture">
-        <img v-bind:src="selectedImage2" @click.once="randomImage2"/>
+        <img v-bind:src="selectedImage2" @click="showPicture2"/>
       </div>
        
        
     </section>
     <router-link class="button" to="/game/1">Retour au salon</router-link>
 
-    <!-- <router-view name="tools">coucou</router-view> -->
+  
     <Tools></Tools>
   </div>
 </template>
@@ -47,70 +46,50 @@ import audioCount from '../services/audioCount';
 import Tools from '../components/Tools.vue';
 
 export default {
-  data() {
-    return {
-      count : 0,
-      images: [
-        'http://via.placeholder.com/350x150',
-        'http://via.placeholder.com/200x140',
-        'http://via.placeholder.com/200x100'
-      ],
-        selectedImage: '',
-        selectedImage1: 'https://static.thenounproject.com/png/393234-200.png',
-        selectedImage2: 'https://static.thenounproject.com/png/393234-200.png'
-    }
-  },
-  components: {
-    Tools
-  },
-  mounted() {
-    // if(localStorage.getItem('image1')){
-    //   try {
-    //     this.selectedImage = JSON.parse(localStorage.getItem('image1'));
-    //   } catch(e) {
-    //     localStorage.removeItem('image1');
-    //   }
-    // }
-
-    if(localStorage.getItem('image1')){
-        this.selectedImage = localStorage.getItem('image1');
-    }
-  },
-    methods: {
-    // showPicture() {
-    //   return false;
-    // },
-    showPicture() {
-      this.selectedImage = 'http://via.placeholder.com/350x150';
-      localStorage.setItem('image1', this.selectedImage);
+    data() {
+      return {
+        count : 0,
+        images: [
+          'http://via.placeholder.com/350x150',
+          'http://via.placeholder.com/200x140',
+          'http://via.placeholder.com/200x100'
+        ],
+          selectedImage: 'https://static.thenounproject.com/png/393234-200.png',
+          selectedImage1: 'https://static.thenounproject.com/png/393234-200.png',
+          selectedImage2: 'https://static.thenounproject.com/png/393234-200.png'
+      }
     },
-    // randomImage() {
-    //   const idx = Math.floor(Math.random() * this.images.length);
-    //   this.selectedImage = this.images[idx];
+    components: {
+      Tools
+    },
+    mounted() {
+      if(localStorage.getItem('image1')){
+          this.selectedImage = localStorage.getItem('image1');
+      }
+      if(localStorage.getItem('image2')){
+          this.selectedImage1 = localStorage.getItem('image2');
+      }
+      if(localStorage.getItem('image3')){
+          this.selectedImage2 = localStorage.getItem('image3');
+      }
+    },
+    methods: {
 
-    //   localStorage.setItem('image1', this.selectedImage);
-    // },
-    // randomImage1 () {
-    //   const idx = Math.floor(Math.random() * this.images.length);
-    //   this.selectedImage1 = this.images[idx];
+      showPicture() {
+        this.selectedImage = this.images[0];
+        localStorage.setItem('image1', this.selectedImage);
+      },
+      showPicture1() {
+        this.selectedImage1 = this.images[1];
+        localStorage.setItem('image2', this.selectedImage1);
+      },
+      showPicture2() {
+        this.selectedImage2 = this.images[2];
+        localStorage.setItem('image3', this.selectedImage2);
+      }
 
-    //   localStorage.setItem('image2', this.selectedImage1);
-    // },
-    randomImage2 () {
-      const idx = Math.floor(Math.random() * this.images.length);
-      this.selectedImage2 = this.images[idx];
-
-    //   localStorage.setItem('image3', this.selectedImage2);
-
-    //   if(localStorage.getItem('image3')) {
-    //     this.selectedImage2 = localStorage.getItem('image3');
-    //   }
-    // }
     }
-    // savePicture() {
-    //   const parsed = JSON.stringify(this.selectedImage);
-    //   localStorage.setItem('image1', parsed);
-    // }
+  
   } 
-} 
+
 </script>
