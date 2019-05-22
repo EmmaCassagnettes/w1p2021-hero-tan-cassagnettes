c9d6650378b73ce63fc03cd7e1343ea2bed15b1b
tanamanda13 <tanamanda13@gmail.com>
2019-05-22 12:09:30 +0200

    Character choice play appear

--- app/components/Character.vue
+++ app/components/Character.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="big-header">
-    <h1>Choisir son personnage</h1>
+    <h1>Choisis ta détective</h1>
     <br />
 
     <section class="players">
-      <div @click="choicePlayer1" class="player player1">Emma</div>
-      <div class="player player2">Amandaaa</div>
+      <div @click="choicePlayer1" class="player player1">Emmaaa</div>
+      <div @click="choicePlayer2" class="player player2">Amandaaa</div>
     </section>
+    
+    <template v-if="showPlay === true">
+      <router-link class="button" to="/game/1">Jouer</router-link>
+    </template>
+
     
 
 
-    <router-link class="button" to="/game/1">Jouer</router-link>
   </div>
 </template>
 
@@ -34,10 +38,40 @@ import data from '../assets/data.json';
 import audioCount from '../services/audioCount';
 
 export default {
+  data() {
+    return {
+      character: ''
+    }
+  },
+  computed: {
+    showPlay() {
+      // if(!localStorage.getItem('character')) {
+      //   //console.log('choix fait');
+      //   return false;
+      // } 
+      if(this.character != '') {
+        return true;
+      }
+    }
+  },
   methods: {
     choicePlayer1(){
       audioCount.increment();
+      this.character = 'Emma';
+      localStorage.setItem('character', 'Emma');
+    },
+     choicePlayer2(){
+      //audioCount.increment();
+      this.character = 'Amanda';
+      localStorage.setItem('character', 'Amanda');
     }
+  //   // showPlay() {
+  //   //   if(localStorage.getItem('character')) {
+  //   //     //console.log('choix fait');
+  //   //     return true;
+  //   //   } 
+  //   // }
+  // }
   }
-};
+}
 </script>
